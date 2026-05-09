11a8557f1affe5a1454d33fb4b67dc3280eec021
linzingandrei <andrei.linzing@gmail.com>
2026-05-09 17:54:46 +0300

    most logic done; missing more checks

--- IpBlocker/IpBlocker.cpp
+++ IpBlocker/IpBlocker.cpp
@@ -1,7 +1,9 @@
 #define CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "stdio.h"
 #include "ws2tcpip.h"
 #include "fwpmu.h"
+#include "conio.h"
 
 #pragma comment(lib, "Fwpuclnt.lib")
 #pragma comment(lib, "Ws2_32.lib")
@@ -22,6 +24,9 @@ WCHAR* GetIpsOfWebsite(WCHAR* nodeName)
     else 
     {
         WCHAR ipStr[NI_MAXHOST];
+        WCHAR* ipStrRes = NULL;
+		ipStrRes = (WCHAR*)malloc(NI_MAXHOST * sizeof(WCHAR));
+
         for (ADDRINFOW* ptr = addrInfoRes; ptr != NULL; ptr = ptr->ai_next) {
             ret = GetNameInfoW(ptr->ai_addr, (socklen_t)ptr->ai_addrlen, ipStr, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
             if (ret == 0)
@@ -30,8 +35,10 @@ WCHAR* GetIpsOfWebsite(WCHAR* nodeName)
             }
         }
 
+		wcscpy_s(ipStrRes, NI_MAXHOST, ipStr);
+
         FreeAddrInfo(addrInfoRes);
-        return ipStr;
+        return ipStrRes;
     }
 }
 
@@ -49,9 +56,96 @@ int main()
 	scanf_s("%ls", nodeName, 1024);
 
 	WCHAR* ipStr = GetIpsOfWebsite(nodeName);
-    free(nodeName);
+    //free(nodeName);
 
-    //FwpmFilterAdd();
+    WCHAR processName[512] = L"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
+
+    FWPM_FILTER filter = { 0 };
+	FWPM_FILTER_CONDITION filterConditions[4] = {0};
+    FWP_BYTE_BLOB* pAppId = NULL;
+    DWORD error = NULL;
+    HANDLE hEngine;
+
+	FwpmEngineOpen(NULL, RPC_C_AUTHN_DEFAULT, NULL, NULL, &hEngine);
+
+    if (processName != NULL)
+    {
+        error = FwpmGetAppIdFromFileName(processName, &pAppId);
+        
+        if (error != ERROR_SUCCESS)
+        {
+            printf("FwpmGetAppIdFromFileName failed with error: %d\n", error);
+            free(processName);
+            WSACleanup();
+            return 1;
+		}
+    }
+
+    if (pAppId)
+    {
+        wprintf(L"%.*s", (int)(pAppId->size / sizeof(WCHAR)), (WCHAR*)pAppId->data);
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+		ZeroMemory(&filterConditions[i], sizeof(FWPM_FILTER_CONDITION));
+    }
+	filterConditions[0].fieldKey = FWPM_CONDITION_ALE_APP_ID;
+	filterConditions[0].matchType = FWP_MATCH_EQUAL;
+	filterConditions[0].conditionValue.type = FWP_BYTE_BLOB_TYPE;
+	filterConditions[0].conditionValue.byteBlob = pAppId;
+
+	filterConditions[1].fieldKey = FWPM_CONDITION_IP_REMOTE_ADDRESS;
+	filterConditions[1].matchType = FWP_MATCH_EQUAL;
+	filterConditions[1].conditionValue.type = FWP_V4_ADDR_MASK;
+
+    FWP_V4_ADDR_AND_MASK* ipAddrMask = NULL;
+    ipAddrMask = (FWP_V4_ADDR_AND_MASK*)malloc(sizeof(FWP_V4_ADDR_AND_MASK));
+    RtlZeroMemory(ipAddrMask, sizeof(FWP_V4_ADDR_AND_MASK));
+	
+    char cIpStr[NI_MAXHOST] = { 0 };
+    size_t convertedChars = 0;
+    wcstombs_s(&convertedChars, cIpStr, NI_MAXHOST, ipStr, _TRUNCATE);
+
+	ipAddrMask->addr = ntohl(inet_addr(cIpStr));
+	ipAddrMask->mask = 0xFFFFFFFF;
+
+	filterConditions[1].conditionValue.v4AddrMask = ipAddrMask;
+
+	ZeroMemory(&filter, sizeof(FWPM_FILTER));
+    filter.displayData.name = nodeName;
+	filter.displayData.description = NULL;
+	filter.layerKey = FWPM_LAYER_ALE_AUTH_CONNECT_V4;
+	filter.weight.type = FWP_EMPTY;
+    filter.numFilterConditions = 2;
+	filter.filterCondition = filterConditions;
+	filter.action.type = FWP_ACTION_BLOCK;
+
+	int res = FwpmFilterAdd(hEngine, &filter, NULL, NULL);
+    if (res != ERROR_SUCCESS)
+    {
+        printf("FwpmFilterAdd failed with error: %d\n", res);
+        free(nodeName);
+        WSACleanup();
+        return 1;
+	}
+
+    while (true)
+    {
+        if (_kbhit())
+        {
+            int c = _getch();
+
+            if (c == 27)
+            {
+                break;
+            }
+        }
+    }
+
+	FwpmFilterDeleteById(hEngine, filter.filterId);
+
+	FwpmEngineClose(hEngine);
 
     WSACleanup();
 
